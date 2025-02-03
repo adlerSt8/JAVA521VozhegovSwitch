23084a4fe617c811935587649fc79029c268f7a2
Андрей <adlerSt@yandex.ru>
2025-02-03 18:20:13 +0300

    Initial commit

---
+++ src/TaskFirst.java
@@ -0,0 +1,57 @@
+import java.util.Scanner;
+
+//TODO: Программа, которая принимает номер месяца от пользователя
+// и выводит текстовое описание месяца с использованием оператора switch.
+public class TaskFirst {
+    public static void main(String[] args) {
+
+        String nameMonth = "";
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Введите номер месяца: ");
+        int monthNumber = scanner.nextInt();
+
+        switch (monthNumber) {
+            case 1:
+                nameMonth = "Январь";
+                break;
+            case 2:
+                nameMonth = "Февраль";
+                break;
+            case 3:
+                nameMonth = "Март";
+                break;
+            case 4:
+                nameMonth = "Апрель";
+                break;
+            case 5:
+                nameMonth = "Май";
+                break;
+            case 6:
+                nameMonth = "Июнь";
+                break;
+            case 7:
+                nameMonth = "Июль";
+                break;
+            case 8:
+                nameMonth = "Август";
+                break;
+            case 9:
+                nameMonth = "Сентябрь";
+                break;
+            case 10:
+                nameMonth = "Октябрь";
+                break;
+            case 11:
+                nameMonth = "Ноябрь";
+                break;
+            case 12:
+                nameMonth = "Декабрь";
+                break;
+            default:
+                System.out.println("Введен неверный номер месяца");
+        }
+
+        System.out.println(nameMonth);
+    }
+}
+
